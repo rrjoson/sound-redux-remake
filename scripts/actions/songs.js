2c0603069cefc13c443dfb12518a82b081a344a8
rrjoson <rr_joson@yahoo.com>
2017-06-27 00:07:37 +0800

    Add Redux Thunk

---
+++ scripts/actions/songs.js
@@ -0,0 +1,19 @@
+import * as types from '../constants/ActionTypes';
+import { CLIENT_ID } from '../constants/Config';
+
+export function fetchSongs() {
+  return dispatch => {
+    return fetch(`http://api.soundcloud.com/tracks?linked_partitioning=1&client_id=${CLIENT_ID}`)
+      .then(response => response.json())
+      .then(json => dispatch(receiveSongs(json)));
+  };
+}
+
+export function receiveSongs(json) {
+  return {
+    type: types.RECEIVE_SONGS,
+    songs: json.collection,
+  }
+}
+
+
