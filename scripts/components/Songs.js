101710c28ca975f659e7e7f19ce909f66fc03ba6
rrjoson <rr_joson@yahoo.com>
2017-06-27 23:22:02 +0800

    Add change active song action

--- scripts/components/Songs.js
+++ scripts/components/Songs.js
@@ -1,11 +1,18 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { changeActiveSong } from '../actions/songs';
 import SongCard from '../components/SongCard';
 
 class Songs extends Component {
   constructor(props) {
     super(props);
     this.renderSongs.bind(this);
+  }
+
+  changeActiveSong(i) {
+    console.log(i);
+    const { dispatch, songs } = this.props;
+    dispatch(changeActiveSong(songs.items[i]));
   }
 
   renderSongs() {
@@ -15,17 +22,23 @@ class Songs extends Component {
     let result = [];
     for (let i = 0; i < items.length; i += chunk) {
       let songCards = items.slice(i, i + chunk).map((song) => {
-        return <div className='col-1-5'><SongCard song={song} /></div>
+        return (
+          <div className='col-1-5' key={song.id}>
+            <SongCard
+              changeActiveSong={this.changeActiveSong.bind(this, i + j)}
+              song={song} />
+          </div>
+        );
       });
 
       if (songCards.length < chunk) {
         for (let j = 0; j < chunk - songCards.length + 1; j++) {
-          songCards.push(<div className='col-1-5'></div>);
+          songCards.push(<div className='col-1-5' key={'song-placeholder-' + j}></div>);
         }
       }
 
       result.push(
-        <div className='songs-row grid'>{songCards}</div>
+        <div className='songs-row grid' key={'songs-row-' + i}>{songCards}</div>
       );
     }
 
