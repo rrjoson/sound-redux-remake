44820204f75e2c80c43284cfc1e162845e464059
rrjoson <rr_joson@yahoo.com>
2017-07-04 20:01:02 +0800

    Add song player seek bar

--- scripts/helpers/Format.js
+++ scripts/helpers/Format.js
@@ -1,5 +1,13 @@
 import { CLIENT_ID } from '../constants/Config';
 
+function padZero(num, size) {
+  let s = num + '';
+  while (s.length < size) {
+    s = '0' + s;
+  }
+  return s;
+}
+
 export function formatSongTitle(s) {
   if (!s) {
     return '';
@@ -8,6 +16,12 @@ export function formatSongTitle(s) {
   const arr = s.replace('–', '-').split(' - ');
 
   return arr[arr.length - 1].split(' (')[0];
+}
+
+export function formatSeconds(num) {
+  const minutes = padZero(Math.floor(num / 60), 2);
+  const seconds = padZero(num % 60, 2);
+  return `${minutes}:${seconds}`;
 }
 
 export function formatStreamUrl(s) {
