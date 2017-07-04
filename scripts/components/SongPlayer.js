29a804735678efa9ae35a6fdb69958d8948297ed
rrjoson <rr_joson@yahoo.com>
2017-07-05 00:16:51 +0800

    Add song player events

--- scripts/components/SongPlayer.js
+++ scripts/components/SongPlayer.js
@@ -1,10 +1,15 @@
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
 import SongDetails from '../components/SongDetails';
-import { formatSeconds, formatStreamUrl } from '../helpers/Format';
+import {formatSeconds, formatStreamUrl} from '../helpers/Format';
 
 class SongPlayer extends Component {
   constructor(props) {
     super(props);
+    this.handleLoadedMetadata = this.handleLoadedMetadata.bind(this);
+    this.handleLoadStart = this.handleLoadStart.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
     this.handlePlay = this.handlePlay.bind(this);
     this.handlePause = this.handlePause.bind(this);
     this.handleTimeUpdate = this.handleTimeUpdate.bind(this);
@@ -14,13 +19,19 @@ class SongPlayer extends Component {
     this.state = {
       currentTime: 0,
       duration: 0,
-      isPlaying: false
+      isPlaying: false,
+      isSeeking: false,
     };
   }
 
   componentDidMount() {
-    this.bindEvents();
-    React.findDOMNode(this.refs.audio).play();
+    const audioElement = React.findDOMNode(this.refs.audio);
+    audioElement.addEventListener('loadedmetadata', this.handleLoadedMetadata, false);
+    audioElement.addEventListener('loadstart', this.handleLoadStart, false);
+    audioElement.addEventListener('play', this.handlePlay, false);
+    audioElement.addEventListener('pause', this.handlePause, false);
+    audioElement.addEventListener('timeupdate', this.handleTimeUpdate, false);
+    audioElement.play();
   }
 
   componentDidUpdate(prevProps) {
@@ -33,27 +44,82 @@ class SongPlayer extends Component {
 
   componentWillUnmount() {
     const audioElement = React.findDOMNode(this.refs.audio);
+    audioElement.removeEventListener('loadedmetadata', this.handleLoadedMetadata, false);
+    audioElement.removeEventListener('loadstart', this.handleLoadStart, false);
     audioElement.removeEventListener('play', this.handlePlay, false);
-    audioElement.removeEventListener('play', this.handlePlay, false);
+    audioElement.removeEventListener('pause', this.handlePause, false);
     audioElement.removeEventListener('timeupdate', this.handleTimeUpdate, false);
   }
 
-  bindEvents() {
+  bindMouseEvents() {
+    document.addEventListener('mousemove', this.handleMouseMove);
+    document.addEventListener('mouseup', this.handleMouseUp);
+  }
+
+  handleLoadedMetadata() {
     const audioElement = React.findDOMNode(this.refs.audio);
-    audioElement.addEventListener('play', this.handlePlay, false);
-    audioElement.addEventListener('pause', this.handlePause, false);
-    audioElement.addEventListener('timeupdate', this.handleTimeUpdate, false);
+    this.setState({
+      duration: Math.floor(audioElement.duration)
+    });
+  }
+
+  handleLoadStart() {
+    this.setState({
+      currentTime: 0,
+      duration: 0,
+    });
+  }
+
+  handleMouseClick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  handleMouseDown(e) {
+    this.bindMouseEvents();
+    this.setState({
+      isSeeking: true,
+    });
+  }
+
+  handleMouseMove(e) {
+    const seekBar = React.findDOMNode(this.refs.seekBar);
+    const diff = e.clientX - seekBar.offsetLeft;
+    const pos = diff < 0 ? 0 : diff;
+    let percent = pos / seekBar.offsetWidth;
+    percent = percent > 1 ? 1 : percent;
+
+    this.setState({
+      currentTime: Math.floor(percent * this.state.duration)
+    });
+  }
+
+  handleMouseUp(e) {
+    if (!this.state.isSeeking) {
+      return;
+    }
+
+    this.unbindMouseEvents();
+    this.setState({
+      isSeeking: false,
+    }, function() {
+      React.findDOMNode(this.refs.audio).currentTime = this.state.currentTime;
+    });
   }
 
   handlePause() {
-    this.setState({ isPlaying: false });
+    this.setState({isPlaying: false});
   }
 
   handlePlay() {
-    this.setState({ isPlaying: true });
+    this.setState({isPlaying: true});
   }
 
   handleTimeUpdate(e) {
+    if (this.state.isSeeking) {
+      return;
+    }
+
     const audioElement = e.currentTarget;
     const currentTime = Math.floor(audioElement.currentTime);
 
@@ -63,7 +129,6 @@ class SongPlayer extends Component {
 
     this.setState({
       currentTime: currentTime,
-      duration: Math.floor(audioElement.duration)
     });
   }
 
@@ -82,8 +147,13 @@ class SongPlayer extends Component {
     }
   }
 
+  unbindMouseEvents() {
+    document.removeEventListener('mousemove', this.handleMouseMove);
+    document.removeEventListener('mouseup', this.handleMouseUp);
+  }
+
   renderDurationBar() {
-    const { currentTime, duration } = this.state;
+    const {currentTime, duration} = this.state;
 
     if (duration !== 0) {
       const width = currentTime/duration * 100;
@@ -91,15 +161,19 @@ class SongPlayer extends Component {
         <div
           className='song-player-seek-duration-bar'
           style={{width: `${width}%`}} >
-          <div className='song-player-seek-handle'></div>
+          <div
+            className='song-player-seek-handle'
+            onClick={this.handleMouseClick}
+            onMouseDown={this.handleMouseDown}>
+          </div>
         </div>
       );
     }
   }
 
   render() {
-    const { song } = this.props;
-    const { currentTime, duration, isPlaying } = this.state;
+    const {song} = this.props;
+    const {currentTime, duration, isPlaying} = this.state;
     const image = song.artwork_url.replace('large', 't300x300');
 
     return (
@@ -107,32 +181,34 @@ class SongPlayer extends Component {
         <audio ref='audio' src={formatStreamUrl(song.stream_url)}></audio>
         <div className='container'>
           <div className='song-player-main'>
-            <div className='song-player-info'>
+            <div className='song-player-main-info'>
               <img className='song-player-image' src={image} />
               <SongDetails title={song.title} username={song.user.username} />
             </div>
-          </div>
-          <div className='song-player-controls'>
-            <div className='song-player-button'>
-              <i className='icon ion-ios-rewind'></i>
+            <div className='song-player-controls'>
+              <div className='song-player-button'>
+                <i className='icon ion-ios-rewind'></i>
+              </div>
+              <div
+                className='song-player-button'
+                onClick={this.togglePlay}>
+                <i className={'icon ' + (isPlaying ? 'ion-ios-pause' : 'ion-ios-play')}></i>
+              </div>
+              <div className='song-player-button'>
+                <i className='icon ion-ios-fastforward'></i>
+              </div>
             </div>
-            <div
-              className='song-player-button'
-              onClick={this.togglePlay}>
-              <i className={'icon ' + (isPlaying ? 'ion-ios-pause' : 'ion-ios-play')}></i>
-            </div>
-            <div className='song-player-button'>
-              <i className='icon ion-ios-fastforward'></i>
-            </div>
-          </div>
-          <div className='song-player-seek' onClick={this.seek}>
-            <div className='song-player-seek-bar'>
-              {this.renderDurationBar()}
-            </div>
-            <div className='song-player-time'>
-              <span>{formatSeconds(currentTime)}</span>
-              <span className='song-player-time-divider'>/</span>
-              <span>{formatSeconds(duration)}</span>
+            <div className='song-player-seek'>
+              <div className='song-player-seek-bar-wrap' onClick={this.seek}>
+                <div ref='seekBar' className='song-player-seek-bar'>
+                  {this.renderDurationBar()}
+                </div>
+              </div>
+              <div className='song-player-time'>
+                <span>{formatSeconds(currentTime)}</span>
+                <span className='song-player-time-divider'>/</span>
+                <span>{formatSeconds(duration)}</span>
+              </div>
             </div>
           </div>
         </div>
@@ -142,7 +218,7 @@ class SongPlayer extends Component {
 }
 
 SongPlayer.propTypes = {
-    song: PropTypes.object
+  song: PropTypes.object
 };
 
 export default SongPlayer;
